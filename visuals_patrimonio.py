ef58972b4d9fe1b5fb63025b9277905ae3de62c4
pabmetrics <pabmetrics@gmail.com>
2026-02-03 20:13:16 +0100

    change heights

--- visuals_patrimonio.py
+++ visuals_patrimonio.py
@@ -168,7 +168,6 @@ def categorias_pie_chart(df):
         plot_bgcolor="#0f172a",
         paper_bgcolor="#0f172a",
         font=dict(color="white"),
-        height=chart_height,
         title=dict(font=dict(color="white")),
         legend=dict(
             orientation="h",
@@ -209,7 +208,6 @@ def create_investment_chart(df):
         plot_bgcolor="#0f172a",  # chart area
         paper_bgcolor="#0f172a",  # outer area
         font=dict(color="white"),
-        height=chart_height,
         title=dict(
             font=dict(
                 color="white"  # font color
